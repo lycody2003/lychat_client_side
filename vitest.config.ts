70a967c47b28a031c7d3ea42f67f181680e071e5
lycody2003 <lycody006@gmail.com>
2026-06-22 22:19:18 +0700

    update ci workflow pipeline

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000,
-    host: true,
-    strictPort: true,
-  },
-})
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+    },
+  }),
+)
